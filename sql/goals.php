e1dcf2385009ae1fc6657e0b18398a6806417f3b
chickenjazz <justinmanuzon413@gmail.com>
2025-07-12 02:35:01 +0800

    added htmlspecialchars()

--- sql/goals.php
+++ sql/goals.php
@@ -5,7 +5,7 @@ try {
     require_once(__DIR__ . '/../sqlConnection/database.php');
 
     //query part
-    $query = "SELECT goal_name, base, current, target, active FROM goals WHERE user_id = 6"; //substite ? with the logged in user //CHANGE 6 TO ? PAG MAY SESSION NA
+    $query = "SELECT * FROM goals WHERE user_id = 6"; //substite ? with the logged in user //CHANGE 6 TO ? PAG MAY SESSION NA
     $stmt = $pdo->prepare($query);
     $stmt->execute(/*[$_SESSION['user_id']]*/); //UNCOMMENT THIS PAG GUMAGANA NA YUNG SESSION
     $results =$stmt->fetchAll(PDO::FETCH_ASSOC); //this will convert the database into an associative array
@@ -13,11 +13,12 @@ try {
     //put the results into an associative array for easier retrieval on goalTracker.php
     foreach($results as $row){
         $goals[] = [
-            'name' => $row['goal_name'], //these are individual columns in the database
-            'baseValue' => (float)$row['base'],
-            'currentValue' => (float)$row['current'],
-            'goalValue' => (float)$row['target'],
-            'active' => (bool)$row['active'],
+            'goal_id' => htmlspecialchars($row['goal_id']),
+            'name' => htmlspecialchars($row['goal_name']), //these are individual columns in the database
+            'baseValue' => htmlspecialchars((float)$row['base']),
+            'currentValue' => htmlspecialchars((float)$row['current']),
+            'goalValue' => htmlspecialchars((float)$row['target']),
+            'active' => htmlspecialchars((bool)$row['active']),
         ];
     }
 }catch(PDOException $e){
